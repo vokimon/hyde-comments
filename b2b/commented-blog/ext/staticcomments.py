072f0395ba367f52772e099afdc236c2db387ce8
David García Garzón <voki@canvoki.net>
2012-12-01 20:51:26 +0100

    Filtering comments on site

--- b2b/commented-blog/ext/staticcomments.py
+++ b2b/commented-blog/ext/staticcomments.py
@@ -1,4 +1,12 @@
-class CommentsPlugin :
+from hyde.plugin import Plugin
+
+class CommentsPlugin(Plugin) :
 	def __init__(self, site) :
-		pass
+		super(CommentsPlugin, self).__init__(site)
+		print "= initializing", self.__class__.__name__
+
+	def begin_site(self) :
+		for r in self.site.content.walk_resources() :
+			if r.source_file.kind != 'comment' : continue
+			r.is_processable = False
 
