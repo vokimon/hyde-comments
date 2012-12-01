29459b60ff89b9bb2c4fb59d6493ee279bc4e90b
David García Garzón <voki@canvoki.net>
2012-12-01 21:42:52 +0100

    test.sh: fixed ansi colors

--- b2b/commented-blog/test.sh
+++ b2b/commented-blog/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf deploy/ && hyde -v gen && find deploy && git diff . && echo -e '\032[33mTEST PASSED\033[0m' || echo -e '\033[31mTEST FAILED\033[0m'
+rm -rf deploy/ && hyde -v gen && find deploy && git diff . && echo -e '\033[32mTEST PASSED\033[0m' || echo -e '\033[31mTEST FAILED\033[0m'
