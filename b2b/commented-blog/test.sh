ebad78ff8b1f82aace050a2df569595ec6215490
David García Garzón <voki@canvoki.net>
2012-12-02 10:21:54 +0100

    Removed tags to separate concerns in tests

--- b2b/commented-blog/test.sh
+++ b2b/commented-blog/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf deploy/ && hyde gen && find deploy && git diff deploy/ && echo -e '\033[32mTEST PASSED\033[0m' || echo -e '\033[31mTEST FAILED\033[0m'
+rm -rf deploy/ && hyde gen && find deploy && git status deploy/ && git diff deploy && echo -e '\033[32mTEST PASSED\033[0m' || echo -e '\033[31mTEST FAILED\033[0m'
